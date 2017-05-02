9c6d4d019c622b889b7ddf936ec00fd99f86beeb
Adam Grzegorzewski <adamg@cakesolutions.net>
2017-05-02 09:26:26 +0100

    Small update

--- Python/multiple_file_tests/test_1suite.py
+++ Python/multiple_file_tests/test_1suite.py
@@ -2,8 +2,9 @@ import pytest
 from conf import *
 
 def test_WD_one():
+	regbutton = 'body > div > div.ng-scope > div.row.ng-scope > div > a'
 	promise = Promise(driver.get('https://cakesolutions.github.io/cake-qa-test/#/'))
-	promise.then(driver.find_element_by_css_selector('body > div > div.ng-scope > div.row.ng-scope > div > a').click())
+	promise.then(driver.find_element_by_css_selector(regbutton).click())
 	assert setUp().current_url == 'https://cakesolutions.github.io/cake-qa-test/#/register'
 
 def test_WD_two():
